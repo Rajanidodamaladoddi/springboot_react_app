dbd9f2c8a5bc3aeab088816b4c8a14cc5197570d
Rajanidodamaladoddi <88015753+Rajanidodamaladoddi@users.noreply.github.com>
2021-07-27 09:02:22 +0530

    Add files via upload

---
+++ springboot-react-app.zip_expanded/springboot-react-app/src/test/java/react/application/springboot/SpringbootReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package react.application.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
